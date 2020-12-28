af537a6c89fe6971cf811faada5396de77a456d3
AugustodeAraujo <poxaugustodantas@gmail.com>
2020-12-28 12:01:18 -0300

    Remove H1 Teste

--- pages/catalogos.js
+++ pages/catalogos.js
@@ -5,7 +5,7 @@ import Layout from 'components/Layout'
 export default function Catalogos () {
     return (
         <Layout>
-           <h1>Teste</h1>
+           
         </Layout>
         
     )
