e2317919c1cc892baf6c1bd9de63fedbaf8fedda
AugustodeAraujo <poxaugustodantas@gmail.com>
2021-03-31 15:13:25 -0300

    catalogo

--- components/Navbar.js
+++ components/Navbar.js
@@ -27,7 +27,7 @@ const NavBar = () => {
                 <NavDropdown.Item >Óleos</NavDropdown.Item>
                 <NavDropdown.Item >Mecânica</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item ><Link href="/catalogos">Catálogos</Link></NavDropdown.Item>
+                <NavDropdown.Item ><Link href="/kaizen_digital.pdf">Catálogo Completo</Link></NavDropdown.Item>
               </NavDropdown>           
             </Nav>
             <a href="https://api.whatsapp.com/send/?phone=5561983660828&text=Ol%C3%A1%2C+eu+gostaria+de+um+or%C3%A7amento.&app_absent=0" target='_blank' className="btn ml-auto" style={{ borderRadius: "2px" }}>
