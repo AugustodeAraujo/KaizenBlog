9f62185d24fd119eda9ad6df8ba3f41027fe8f18
Augusto de Araujo <66418562+AugustodeAraujo@users.noreply.github.com>
2021-08-27 17:56:21 -0300

    Whatsapp Number Update

--- components/Navbar.js
+++ components/Navbar.js
@@ -31,7 +31,7 @@ const NavBar = () => {
                 <a href="https://drive.google.com/file/d/1s3f_HNw0pK2J5SNq-fayciRl-WzN4n3w/view" target='_blank' style={{marginLeft: "8px"}}>Catálogo Completo</a>
               </NavDropdown>           
             </Nav>
-            <a href="https://api.whatsapp.com/send/?phone=5561983660828&text=Ol%C3%A1%2C+eu+gostaria+de+um+or%C3%A7amento.&app_absent=0" target='_blank' className="btn ml-auto" style={{ borderRadius: "2px" }}>
+            <a href="https://api.whatsapp.com/send/?phone=556183660828&text=Ol%C3%A1%2C+eu+gostaria+de+um+or%C3%A7amento.&app_absent=0" target='_blank' className="btn ml-auto" style={{ borderRadius: "2px" }}>
                 <button type="button" className="btn btn-primary my-3"><AiOutlineWhatsApp className="m-1" /> SOLICITAR ORÇAMENTO</button>
               </a >
           </Navbar.Collapse>
