6d8a6219c2ac6c7879d46a97a5f765977d8f56c1
AugustodeAraujo <poxaugustodantas@gmail.com>
2021-11-09 22:52:12 -0300

    Google Tags & Div Entregas

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,34 @@ export default function Home() {
         <meta property="og:url" content="http://www.kaizenautopecas.com.br" key="ogurl" />
         <meta property="og:site_name" content="Kaizen - A Casa da Autopeça" key="ogsitename" />
         <meta property="og:title" content="Kaizen - A Casa da Autopeça" key="ogtitle" />
+        
         <meta property="og:description" content="Kaizen - A Casa da Autopeça. Todas as linhas de autopeça para o seu carro. Temos mais de 60.000 autopeças diferentes em nosso estoque." key="ogdesc" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-697675115"></script>
+        <script
+          async dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'AW-697675115');`
+          }}
+        />
+        <script
+          async dangerouslySetInnerHTML={{
+            __html: `window.addEventListener("DOMContentLoaded", function () {
+            console.log("Dom Ready");
+            document.querySelectorAll('a[href*="api.whatsapp.com"]')
+              .forEach(function (wp) {
+              wp.addEventListener("click", function () {
+                console.log("Sent Request Quote");
+              });
+              gtag("event", "conversion", {
+                send_to: "AW-697675115/IFoRCIqz4P4CEOva1swC"
+              });
+            });
+          });`
+        }}
+      />    
       </Head>
       <Layout>
         <Hero />
